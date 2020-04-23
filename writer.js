15adc31e77f2f1529fae4c9160bee0d379cdb321
nandemotoken <nandemotoken@gmail.com>
2020-04-23 22:45:01 +0000

    modify

--- writer.js
+++ writer.js
@@ -61,7 +61,7 @@ mycontract = new web3js.eth.Contract(ABI , Address);
 //1.00-beta.36と表示されるのが正しい
 console.log( "writer.js_is_checking_web3.js_version..." + Web3.version );
 
-
+console.log(document.getElementById('id100'))
 
 // var tmp_info;
 // function w_function1( hidari , migi ){ mycontract.methods.setnumber(tmp_info).send({from: migi[0] });}
