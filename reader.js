f2602112b3bec09975966add1b19fa4d7df6336e
nandemotoken <nandemotoken@gmail.com>
2020-04-23 16:24:46 +0000

    change codes

--- reader.js
+++ reader.js
@@ -1,5 +1,60 @@
 //環境設定
-var Address = "0x66325a60194e087388ac11c92909a5c5ba170876";
+
+//remixのボタンを押して取得したコントラクトアドレスを貼り付け
+const Address = "0x66325a60194e087388ac11c92909a5c5ba170876";
+
+//remixのボタンを押して取得したABIを貼り付け
+const ABI = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "n",
+				"type": "uint256"
+			}
+		],
+		"name": "setnumber",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "get3",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getnumber",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
+//--------------メイン処理---------------
+
+
+//------web3.jsの読み込み待ちのため、window.onloadにメイン処理を入れる--------
+
+window.onload = function (){
 
 //metamaskの準備
 web3js = new Web3(web3.currentProvider);	
@@ -21,6 +76,8 @@ ethereum.enable().then(console.log);
 //mycontract.methods.getnumber().call().then(r_functionN);
 //...solidityの関数呼出の結果が、r_functionNで処理される。
 //---------------------------------------------------
+
+}
 
 function r_function1( r_result1 ){ console.log(r_result1); }
 mycontract.methods.get3().call().then(r_function1);
